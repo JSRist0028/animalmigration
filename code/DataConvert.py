22efa2a8d18c1200d46c8e0ade6b8271f70f7d78
JSRist0028 <77699095+JSRist0028@users.noreply.github.com>
2021-04-10 11:05:47 -0400

    Add files via upload

--- code/DataConvert.py
+++ code/DataConvert.py
@@ -12,10 +12,23 @@ import csv
 
 # import raw dataset from movebank (if you would like to do different dataset, change file reference) and create .csv for input into neural net
 
-rawdata = pandas.read_csv("https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/Azores%20Great%20Whales%20Satellite%20Telemetry%20Program%20.csv")
-sortdata = rawdata.sort_values(by = ['tag-local-identifier', 'timestamp'])
+    # Whale data link: "https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/Azores%20Great%20Whales%20Satellite%20Telemetry%20Program%20.csv"
+    # Goose data link: "https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/barnacle_geese_data_matrix.csv"
+    
+    # 'tag-local-identifier', 'location-lat', 'location-long' for whale
+    # 'birdID', 'latitude', 'longitude' for goose
+        
+animalID = 'birdID'
+latID = 'latitude'
+longID = 'longitude'
 
-with open('C:/Users/Justin Rist/Desktop/WhaleData.csv', mode = 'w', newline = '') as netdata:
+
+rawdata = pandas.read_csv("https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/barnacle_geese_data_matrix.csv")
+sortdata = rawdata.sort_values(by = [animalID, 'timestamp'])
+
+    # change created file name/path here
+
+with open('C:/Users/Justin Rist/Desktop/GooseData.csv', mode = 'w', newline = '') as netdata:
     netdata_writer = csv.writer(netdata, delimiter = ',')
     
     # headers for new csv
@@ -24,14 +37,14 @@ with open('C:/Users/Justin Rist/Desktop/WhaleData.csv', mode = 'w', newline = ''
     
     # iterate through unique animals
 
-    for animal in sortdata['tag-local-identifier'].unique():
+    for animal in sortdata[animalID].unique():
         
-        #iterate through timestamps of unique animal
+        # iterate through timestamps of unique animal
         
-        obscount = sortdata[sortdata['tag-local-identifier'] == animal].shape[0]
-        obslist = sortdata.index[sortdata['tag-local-identifier'] == animal].tolist()
+        obscount = sortdata[sortdata[animalID] == animal].shape[0]
+        obslist = sortdata.index[sortdata[ animalID] == animal].tolist()
         
         for obs in range(1, obscount, 1):
             
-            netdata_writer.writerow([animal, sortdata['timestamp'][obslist[obs]], sortdata['location-lat'][obslist[obs]], sortdata['location-long'][obslist[obs]],
-                                    sortdata['timestamp'][obslist[obs - 1]], sortdata['location-lat'][obslist[obs - 1]], sortdata['location-long'][obslist[obs - 1]]])
+            netdata_writer.writerow([animal, sortdata['timestamp'][obslist[obs]], sortdata[latID][obslist[obs]], sortdata[longID][obslist[obs]],
+                                    sortdata['timestamp'][obslist[obs - 1]], sortdata[latID][obslist[obs - 1]], sortdata[longID][obslist[obs - 1]]])
