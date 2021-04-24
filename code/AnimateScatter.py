081c08d87614b76df659e03e57265f9042e9de43
JSRist0028 <77699095+JSRist0028@users.noreply.github.com>
2021-04-24 10:08:22 -0400

    Add files via upload

--- code/AnimateScatter.py
+++ code/AnimateScatter.py
@@ -11,11 +11,27 @@ import matplotlib.animation as animation
 
 # change following links to select data to graph
 
-data = pd.read_csv('https://raw.githubusercontent.com/JSRist0028/animalmigration/main/longtest_y.csv', header = None)
-data2 = pd.read_csv('https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/testing_y.csv', header = None)
+data = pd.read_csv('https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_predict_temp.csv', header = None)
+data2 = pd.read_csv('https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_actual_temp.csv', header = None)
+
+# Goose no temp:https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/goose_predict_notemp.csv
+#               https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/goose_actual_notemp.csv
+
+# Goose temp:   https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/goose_predict_temp.csv
+#               https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/goose_actual_temp.csv
+
+# Swan no temp: https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_predict_notemp.csv
+#               https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_actual_notemp.csv
+
+# Swan temp:    https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_predict_temp.csv
+#               https://raw.githubusercontent.com/JSRist0028/animalmigration/main/data/swan_actual_temp.csv
 
 fig = plt.figure()
-ax = plt.axes(xlim=(50, 75), ylim=(0, 60))
+
+# Goose long lat: xlim=(50, 75), ylim=(0, 60)
+# Swan long lat: xlim=(30, 75), ylim=(-500, -50)
+
+ax = plt.axes(xlim=(30, 75), ylim=(-500, -50))
 line = ax.scatter([], [], c = 'orange')
 line2 = ax.scatter([], [], c = 'blue')
 
